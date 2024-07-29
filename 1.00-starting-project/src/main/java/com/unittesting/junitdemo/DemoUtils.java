8abb65e30b5407a0829bfdb8f0f857aeccd23c98
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-07-29 10:04:15 +0700

    add: testEqualsAndNotEquals

---
+++ 1.00-starting-project/src/main/java/com/unittesting/junitdemo/DemoUtils.java
@@ -0,0 +1,8 @@
+package com.unittesting.junitdemo;
+
+public class DemoUtils {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ 1.00-starting-project/src/test/java/com/unittesting/junitdemo/DemoUtilsTest.java
@@ -0,0 +1,17 @@
+package com.unittesting.junitdemo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DemoUtilsTest {
+
+    @Test
+    @DisplayName("Add two numbers")
+    void testEqualsAndNotEquals() {
+        DemoUtils demoUtils = new DemoUtils();
+        assertEquals(6, demoUtils.add(2, 4), "2+4 must be equal 6");
+        assertNotEquals(7, demoUtils.add(1, 9), "1+9 must be not equal 7");
+    }
+}
